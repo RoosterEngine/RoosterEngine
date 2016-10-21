7e18beba6de1145cb3447e15126d4099d8d23388
dan-rusu-inc <roosterdan@gmail.com>
2016-10-20 22:20:16 -0400

    Improve the precision of the rate counter

--- src/Utilities/RateCounter.java
+++ src/Utilities/RateCounter.java
@@ -6,6 +6,8 @@ package Utilities;
  * Created by Dan on 10/19/2016.
  */
 public class RateCounter {
+   private static final long NANOS_PER_MILLI = 1000000;
+   private static final long NANOS_PER_SECOND = NANOS_PER_MILLI * 1000;
    private long totalTickCount;
    private long startTime, lastTickTime;
    private LongQueue tickTimes;
@@ -19,17 +21,17 @@ public class RateCounter {
       tickTimes = new LongQueue(60);
       //add an artificial tick a sixtieth of a second in the past which will only affect the
       // current tick rate during the first second
-      tickTimes.enqueue(System.currentTimeMillis() - 16);
+      tickTimes.enqueue(System.nanoTime() - 16 * NANOS_PER_MILLI);
    }
 
    /**
     * Registers a new action.
     */
    public void registerTick() {
-      long now = System.currentTimeMillis();
+      long now = System.nanoTime();
       lastTickTime = now;
       tickTimes.enqueue(now);
-      long cutoff = now - 1000;//only keep samples for the last second
+      long cutoff = now - NANOS_PER_SECOND;//only keep samples for the last second
       //keep at least 2 samples since the tick rate is calculated from the last tick time (not
       // the current time)
       while (tickTimes.size() > 2 && tickTimes.peek() < cutoff) {
@@ -43,7 +45,7 @@ public class RateCounter {
     * instance
     */
    public double getAverageTickRate() {
-      return totalTickCount / ((double) (lastTickTime - startTime));
+      return totalTickCount * 1000.0 / (System.currentTimeMillis() - startTime);
    }
 
    /**
@@ -54,6 +56,6 @@ public class RateCounter {
    public double getCurrentTickRate() {
       //the number of ticks that occurred in the last second.  Multiply by 1000 since the times
       //are in milliseconds
-      return tickTimes.size() * 1000.0 / (lastTickTime - tickTimes.peek());
+      return tickTimes.size() * NANOS_PER_SECOND / (double) (lastTickTime - tickTimes.peek());
    }
 }
